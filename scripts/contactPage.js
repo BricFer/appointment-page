92733035012b19cb18bca3d46d52dee8363fd95d
BricFern <heybri_89@hotmail.com>
2024-08-08 13:03:54 +0200

    change number

--- scripts/contactPage.js
+++ scripts/contactPage.js
@@ -3,7 +3,7 @@ let whatsapp = document.querySelector('#whatsapp');
 
 //Constantes para crear la ruta y el mensaje de WhatsApp
 const wpMessage = "¡Hola!\nEsto es una prueba de mensaje integrando WhatsApp a la web";
-const wpPhone = "+34655408136";
+const wpPhone = "+34987654321";
 
 //encodeURIComponent(); se utiliza para codificar una URI (Uniform Resource Identifier) de manera que todos los caracteres especiales se conviertan en un formato seguro para ser utilizados en una URL. Este formato seguro es conocido como percent-encoding o URL encoding, y es necesario para garantizar que los datos se transmitan correctamente a través de la web.
 let mensajeCodificado = encodeURIComponent(wpMessage);
